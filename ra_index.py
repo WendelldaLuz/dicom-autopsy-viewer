15bb9173d69f21b8b08b90b103ee2e56438ed5c2
WendelldaLuz <wenndell.luz@gmail.com>
2025-09-16 16:10:29 -0300

    Delete ra_index.py

--- ra_index.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def show(dicom_data, image_array):
-    st.header("RA Index")
-    st.write("Aqui você pode implementar o cálculo e visualização do RA Index.")
-    st.info("Funcionalidade RA Index em desenvolvimento.")
-  
