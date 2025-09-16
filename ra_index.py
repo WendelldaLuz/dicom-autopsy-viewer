f2f33c5fed15aca6b2eaf7299d85d3b3a3b208b7
WendelldaLuz <wenndell.luz@gmail.com>
2025-09-16 14:52:59 -0300

    Create ra_index.py

---
+++ ra_index.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def show(dicom_data, image_array):
+    st.header("RA Index")
+    st.write("Aqui você pode implementar o cálculo e visualização do RA Index.")
+    st.info("Funcionalidade RA Index em desenvolvimento.")
+  
