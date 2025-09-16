ee9b3bf52f15729e1c45463825252ae200d127fd
WendelldaLuz <wenndell.luz@gmail.com>
2025-09-16 19:39:48 -0300

    Delete relatorios.py

--- relatorios.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def show(dicom_data, image_array):
-    st.header("Relatórios")
-    st.write("Geração e visualização de relatórios baseados nas análises.")
-    st.info("Funcionalidade de relatórios em desenvolvimento.")
